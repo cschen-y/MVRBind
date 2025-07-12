7feb89e301ff6e05f862e4625f59d556548dc9b7
cschen-y <244712246@csu.edu.cn>
2025-07-12 21:11:28 +0800

    Update setup_linux.sh

--- setup_linux.sh
+++ setup_linux.sh
@@ -23,4 +23,4 @@ pip install pillow -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 
 echo ">>> Verify Installation:"
-python -c "import torch; import torch_geometric; print('PyTorch:', torch.__version__, '| CUDA:', torch.version.cuda, '| PyG PyG Successfully installed!')"
+python -c "import torch; import torch_geometric; print('PyTorch:', torch.__version__, '| CUDA:', torch.version.cuda, '| PyG Successfully installed!')"
